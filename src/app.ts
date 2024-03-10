1fb72aed73e8c638cd57488e5827bd3e174434ae
90kid <januszakahipis@wp.pl>
2024-03-10 10:49:54 +0100

    Remove useless routes

--- src/app.ts
+++ src/app.ts
@@ -5,15 +5,7 @@ const PORT: number = parseInt(process.env.PORT || '3000');
 
 app.get('/', (req: Request, res: Response) => {
   console.log(`Dawid!`);
-  res.send('Hello, Docker!!');
-});
-
-app.get('/dawid', (req: Request, res: Response) => {
-  res.send('Dawid');
-});
-
-app.get('/test', (req: Request, res: Response) => {
-  res.send('Test');
+  res.send('Hello, Docker!');
 });
 
 app.listen(PORT, () => {
